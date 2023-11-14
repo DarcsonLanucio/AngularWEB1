3e717c5a33e1460cb2061e0d7c9de972f0a430be
DarcsonLanucio <dodosan100@gmail.com>
2023-11-14 20:15:29 -0300

    feat: add body and nav bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { ListaPokeComponent } from './lista-poke/lista-poke.component';
 import { PokeCardComponent } from './poke-card/poke-card.component';
 import {PokemonService} from "../_services/pokemon.service";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { BodyComponent } from './body/body.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
 @NgModule({
   declarations: [
     AppComponent,
     ListaPokeComponent,
-    PokeCardComponent
+    PokeCardComponent,
+    BodyComponent,
+
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    NavbarComponent
   ],
   providers: [
 
