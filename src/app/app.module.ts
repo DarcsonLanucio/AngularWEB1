679e3979e969f5b8aca0d0cae85089a2c09aa02a
DarcsonLanucio <dodosan100@gmail.com>
2023-11-14 20:30:35 -0300

    fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { ListaPokeComponent } from './lista-poke/lista-poke.component';
-import { PokeCardComponent } from './poke-card/poke-card.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {ListaPokeComponent} from './lista-poke/lista-poke.component';
+import {PokeCardComponent} from './poke-card/poke-card.component';
 import {PokemonService} from "../_services/pokemon.service";
-import {HttpClient, HttpClientModule} from "@angular/common/http";
-import { BodyComponent } from './body/body.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import {HttpClientModule} from "@angular/common/http";
+import {BodyComponent} from './body/body.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatIconModule} from "@angular/material/icon";
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,8 +21,6 @@ import {MatIconModule} from "@angular/material/icon";
     HttpClientModule,
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
     NavbarComponent
   ],
   providers: [
@@ -33,4 +29,5 @@ import {MatIconModule} from "@angular/material/icon";
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
